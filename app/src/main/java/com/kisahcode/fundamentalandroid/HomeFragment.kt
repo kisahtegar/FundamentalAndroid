14542e636e08b96d8d4c006cc7728fbd9e8c559c
kisahtegar <code.kisahtegar@gmail.com>
2024-03-07 23:36:51 +0700

    Adding fragment activity.

---
+++ app/src/main/java/com/kisahcode/fundamentalandroid/HomeFragment.kt
@@ -0,0 +1,60 @@
+package com.kisahcode.fundamentalandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+/**
+ * A simple [Fragment] subclass representing the home screen of the application.
+ * This fragment displays the main interface with options for users to navigate through the app.
+ */
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * In HomeFragment there is an onCreateView() method where the interface layout is defined and
+     * transformed from a layout in the form of an xml file into a view object using the inflate()
+     * method.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's UI should be attached to.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        /**
+         * Inflater.inflate() is an object from LayoutInflater which functions to change the XML
+         * layout into the form of a ViewGroup object or widget by calling the inflate() method.
+         * Just like setContentView in Activity, the inflate function here is to display the layout
+         * of the Fragment, where the layout displayed here is fragment_home.
+         */
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    /**
+     * Called immediately after onCreateView() has returned, providing a view to this fragment's layout.
+     *
+     * @param view The View returned by onCreateView().
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize and set up the button for navigating to categories
+        val btnCategory:Button = view.findViewById(R.id.btn_category)
+        btnCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/kisahcode/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/kisahcode/fundamentalandroid/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.kisahcode.fundamentalandroid
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
+/**
+ * The main activity of the application. This activity serves as the entry point of the app and hosts various fragments.
+ * It handles the initialization of the UI components and manages fragment transactions.
+ */
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Called when the activity is starting. This is where most initialization should go.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize FragmentManager to manage fragments within the activity
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment() // Instance the HomeFragment
+        // Check if the HomeFragment is already added to the activity
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        // If the HomeFragment is not already added, add it to the activity
+        if (fragment !is HomeFragment) {
+            // Log the name of the fragment being added for debugging purposes
+            Log.d("MyFlexibleFragment", "Fragment Name:" + HomeFragment::class.java.simpleName)
+
+            // Begin a transaction to add the HomeFragment to the activity
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
         }
     }
 }
