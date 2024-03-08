ee0477314af6cce0357f58d3e0b78a5957e2d5de
kisahtegar <code.kisahtegar@gmail.com>
2024-03-08 22:06:29 +0700

    Adding Option Menu in AppBar.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Fundamental Android
 
-This repository used for learning:
+Learn about Navigation specially Option Menu in AppBar.
+
+| Nama Project                  | MyAppBar                       |
+|-------------------------------|--------------------------------|
+| Target & Minimum Target SDK   | Phone and Tablet, API level 24 |
+| Tipe Activity                 | Empty Views Activity           |
+| Activity Name                 | MainActivity                   |
+| Language                      | Kotlin                         |
+| Build Configuration Language  | Kotlin DSL                     |
+
--- app/src/main/java/com/kisahcode/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/kisahcode/fundamentalandroid/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.kisahcode.fundamentalandroid
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.kisahcode.fundamentalandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    /**
+     * Called when the activity is starting.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down then
+     * this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     * Note: Otherwise, it is null.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set click listener for the top app bar menu items
+        binding.topAppBar.setOnMenuItemClickListener{
+            when (it.itemId) {
+                R.id.menu1 -> {
+                    // Replace current fragment with MenuFragment
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, MenuFragment())
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+                R.id.menu2 -> {
+                    // Start MenuActivity
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
