d5f4cae045f495a757f09dd297f6324f2d03ab47
kisahtegar <code.kisahtegar@gmail.com>
2024-03-12 12:42:18 +0700

    Implement Bottom Navigation with NavigationComponent

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Fundamental Android
+# Bottom Navigation with NavigationComponent
 
-This repository used for learning:
+Learn about Navigation specially Bottom Navigation with NavigationComponent.
+
+| Nama Project                  | MyBottomNavigation                |
+|-------------------------------|-----------------------------------|
+| Target & Minimum Target SDK   | Phone and Tablet, API level 24    |
+| Tipe Activity                 | Bottom Navigation Views Activity  |
+| Activity Name                 | MainActivity                      |
+| Language                      | Kotlin                            |
+| Build Configuration Language  | Kotlin DSL                        |
+
+<img src="preview_1.png" alt="Preview 1" width="200" height="400">
+<img src="preview_2.png" alt="Preview 2" width="200" height="400">
--- app/src/main/java/com/kisahcode/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/kisahcode/fundamentalandroid/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.kisahcode.fundamentalandroid
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kisahcode.fundamentalandroid.databinding.ActivityMainBinding
 
+/**
+ * The main activity of the application.
+ * This activity hosts the bottom navigation view and manages navigation between different fragments.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Inflating layout and setting content view
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Finding the bottom navigation view
+        val navView: BottomNavigationView = binding.navView
+
+        // Finding the navController associated with the NavHostFragment
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
+            )
+        )
+
+        // Setting up action bar with the navController and app bar configuration
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Setting up bottom navigation view with the navController
+        navView.setupWithNavController(navController)
     }
 }
